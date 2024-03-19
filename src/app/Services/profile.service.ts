9ff27d9c3cee8ff0ec8e595c716c1a3dc01f5f8f
Sebin Sonu Jose <sebinsonu7@gmail.com>
2024-03-19 17:07:00 +0530

    all data fetched

--- src/app/Services/profile.service.ts
+++ src/app/Services/profile.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+import { Profile } from "../Models/profile.model";
 
 
 @Injectable({providedIn:"root"})
@@ -11,4 +13,19 @@ export class ProfileService{
     createProfile(profile){
        return this.http.post('https://profilebuilderbyssj-default-rtdb.firebaseio.com/profiles.json',profile)
     }
+
+    getProfiles(){
+        return this.http.get <{[key:string]:Profile}>('https://profilebuilderbyssj-default-rtdb.firebaseio.com/profiles.json')
+       .pipe(map((res)=>{
+       const profiles=[];  
+       for(const key in res){
+         if(res.hasOwnProperty(key)){
+            profiles.push({...res[key],id:key})
+         }
+         }
+         return profiles
+       }))
+       
+           
+       }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Profile } from '../Models/profile.model';
+import { ProfileService } from '../Services/profile.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  allProfiles:Profile[]=[];
+  isFetching:boolean=false;
+
+  constructor(private ProfileService:ProfileService){
+
+  }
+  ngOnInit(): void {
+    this.fetchProfiles();
+      
+  }
+  private fetchProfiles(){
+    this.isFetching=true;
+    this.ProfileService.getProfiles().subscribe((profiles)=>{
+      this.allProfiles=profiles;
+      console.log(this.allProfiles);
+      this.isFetching=false
+      
+
+    })
+  }
 
 }
