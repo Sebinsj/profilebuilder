b915141c1b4e1304058ae68de77e0328b1154f04
Sebin Sonu Jose <sebinsonu7@gmail.com>
2024-03-19 11:08:19 +0530

    basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ViewComponent } from './view/view.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'Home',component:HomeComponent},
+  {path:'Registration',component:RegistrationComponent},
+  {path:'ViewProfile/:id',component:ViewComponent},
+  {path:'EditProfile/:id',component:EditComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegistrationComponent,
+    EditComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
